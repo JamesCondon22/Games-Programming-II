bed22fd087c88e8827014e8fd6878525313707bb
James Condon <c00207200@itcarlow.ie>
2018-10-04 12:17:41 +0100

    FSM complete

---
+++ FSM/FSM/Climbing.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "State.h"
+#include "Idle.h"
+
+class Climbing : public State
+{
+public:
+	Climbing() {};
+	~Climbing() {};
+	void idle(Animation* a);
+
+};
---
+++ FSM/FSM/InputHandler.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Animation.h"
+
+#include <SDL.h>
+
+class InputHandler
+{
+public: 
+	InputHandler();
+	~InputHandler();
+		
+	void handleInput(SDL_Event & event);
+
+private:
+
+
+	Animation * fsm;
+
+	
+}; 
