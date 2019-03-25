ffb23edabfac8b0c4b70b33cae81974298bf6c15
James Condon <c00207200@itcarlow.ie>
2019-03-25 12:08:19 +0000

    added petersons

--- Petersons/main.cpp
+++ Petersons/main.cpp
@@ -6,7 +6,7 @@
 int turn;
 
 
-const int AMOUNT = 4;
+const int AMOUNT = 5;
 
 int in[AMOUNT];
 int last[AMOUNT];
@@ -15,28 +15,35 @@ bool flag[4];
 
 std::vector<std::thread> m_threads;
 
+/// <summary>
+/// function to thread n processes 
+/// </summary>
+/// <param name="s">the current number</param>
 void CS(void * s)
 {
-
+	//casts s(current number) to an interger 
 	int n = (int)s;
 
 	while (true)
 	{
-		for (int j = 1; j <= AMOUNT; j++)
+		for (int j = 0; j < AMOUNT; j++)
 		{
-			in[n] = j;
-			last[j] = n;
 
-			for (int k = 0; k <= AMOUNT; k++)
+			in[n] = j;//sets the current
+			last[j] = n;//sets the last 
+
+			for (int k = 0; k < AMOUNT; k++)
 			{
 				if (k != n)
 				{
+					//wait if process k is in a higher stage and i was last to enter stage j
 					while (in[k] >= in[n] && last[j] == n);
 				}
 			}
 		}
-		std::cout << "Count: " << n << std::endl;
-		in[n] = 0;
+		//critical section
+		std::cout << "PROCESS: " << n << std::endl;
+		in[n] = -1;
 	}
 }
 
@@ -85,7 +92,7 @@ int main()
 		m_threads.push_back(std::thread(CS, (void*)i));
 	}
 	
-	for (int i = 0; i <= AMOUNT; i++)
+	for (int i = 0; i < AMOUNT; ++i)
 	{
 		m_threads[i].join();
 	}
